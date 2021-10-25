5ac311eb39df5786162beefb023c166689599334
martinmunillas <martinmunillas@gmail.com>
2021-10-24 21:24:08 -0300

    feat: sign in sign up

---
+++ Components/Logo.swift
@@ -0,0 +1,20 @@
+//
+//  Logo.swift
+//  Muniflix
+//
+//  Created by Martin Munilla on 24-10-21.
+//
+
+import SwiftUI
+
+struct Logo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Logo_Previews: PreviewProvider {
+    static var previews: some View {
+        Logo()
+    }
+}
